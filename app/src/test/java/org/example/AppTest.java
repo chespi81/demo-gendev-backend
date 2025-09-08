0b913fb542cea4f19f01a17a2a62d7dd74973eb6
rsanmartin <rsanmartin@tinet.cl>
2025-09-08 18:07:29 -0300

    Elimina hello world.

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,11 +4,7 @@
 package org.example;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+    // Tests for App class will be added here
 }
